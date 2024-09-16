04a8fc46dc41ec759ada6d08b327dc8161d0c51a
JJTech0130 <jjtech@jjtech.dev>
2024-09-16 16:18:03 -0400

    fix build

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+if [[ -z "$LIBRARY_PATH" && -d /opt/homebrew ]]; then
+	echo "Using /opt/homebrew for LIBRARY_PATH and CPATH"
+	export LIBRARY_PATH=/opt/homebrew/lib
+	export CPATH=/opt/homebrew/include
+fi
 MAUTRIX_VERSION=$(cat go.mod | grep 'maunium.net/go/mautrix ' | awk '{ print $2 }' | head -n1)
 GO_LDFLAGS="-s -w -X main.Tag=$(git describe --exact-match --tags 2>/dev/null) -X main.Commit=$(git rev-parse HEAD) -X 'main.BuildTime=`date -Iseconds`' -X 'maunium.net/go/mautrix.GoModVersion=$MAUTRIX_VERSION'"
-go build -ldflags="$GO_LDFLAGS" ./cmd/mautrix-twilio "$@"
+go build -ldflags="$GO_LDFLAGS" ./cmd/imessage-rustpush "$@"
