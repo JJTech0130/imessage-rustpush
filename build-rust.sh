823e05b4de41e71dc6d5be72ad0abfc10c3bb295
JJTech0130 <jjtech@jjtech.dev>
2024-09-17 14:55:16 -0400

    install go bindgen in sh script

--- build-rust.sh
+++ build-rust.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 cd pkg/rustpushgo
 cargo build --release
+
+# Check if the uniffi-bindgen-go binary is installed, if not install it
+if ! command -v uniffi-bindgen-go &> /dev/null
+then
+    cargo install uniffi-bindgen-go --git https://github.com/NordSecurity/uniffi-bindgen-go --tag v0.2.1+v0.25.0
+fi
 uniffi-bindgen-go target/release/librustpushgo.a --library --out-dir ..
