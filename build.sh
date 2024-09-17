62d6b61ea3d344754ef9037f9c0c52a2f96a952c
JJTech0130 <jjtech@jjtech.dev>
2024-09-16 21:39:49 -0400

    clean build script

--- build.sh
+++ build.sh
@@ -3,15 +3,17 @@ set -e
 
 ./build-rust.sh
 
+# Fix for libolm through homebrew
 if [[ -z "$LIBRARY_PATH" && -d /opt/homebrew ]]; then
-	echo "Using /opt/homebrew for LIBRARY_PATH and CPATH"
-	export LIBRARY_PATH=/opt/homebrew/lib
-	export CPATH=/opt/homebrew/include
+	export LIBRARY_PATH=/opt/homebrew/lib:$LIBRARY_PATH
+	export CPATH=/opt/homebrew/include:$CPATH
 fi
+
 export LIBRARY_PATH=.:$LIBRARY_PATH
 
 cp -f pkg/rustpushgo/target/release/librustpushgo.a .
 
+# Build mautrix bridge
 MAUTRIX_VERSION=$(cat go.mod | grep 'maunium.net/go/mautrix ' | awk '{ print $2 }' | head -n1)
 GO_LDFLAGS="-s -w -X main.Tag=$(git describe --exact-match --tags 2>/dev/null) -X main.Commit=$(git rev-parse HEAD) -X 'main.BuildTime=`date -Iseconds`' -X 'maunium.net/go/mautrix.GoModVersion=$MAUTRIX_VERSION'"
 go build -ldflags="$GO_LDFLAGS" ./cmd/imessage-rustpush "$@"
